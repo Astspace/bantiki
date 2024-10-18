b09fcebbc0ad8b3d06dc64ce9e2572d1573f37e4
Astspace <asteroid.space@gmail.com>
2024-10-18 16:50:20 +0300

    18.10.2024

---
+++ bantiki/main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, register_converter
+from . import views, converters
+
+register_converter(converters.FourDigitYearConverter, "conv_year")
+
+urlpatterns = [
+    path('', views.index),
+    path('<conv_year:year>', views.years),
+    path('<int:cat_id>', views.index)
+]
--- bantiki/main/views.py
+++ bantiki/main/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request, cat_id=None): #HttpReqest
+    return HttpResponse(f"Главная страница, cat_id = {cat_id}")
+
+def years(request, year):
+    return HttpResponse(f"Тест прошел удачно, год: {year}")
